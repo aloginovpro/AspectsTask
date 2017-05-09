6eb7df4766f0ffa43793d3827d4aebf0a58db003
Alexey <aloginovpro@gmail.com>
2017-05-09 11:45:30 +0300

    Add annotation configured aspects

--- src/main/java/com/vbelova/aspect/Aspect.java
+++ src/main/java/com/vbelova/aspect/Aspect.java
@@ -2,30 +2,38 @@ package com.vbelova.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StopWatch;
 
+@org.aspectj.lang.annotation.Aspect
 public class Aspect {
     private static final Logger logger = LoggerFactory.getLogger(Aspect.class);
+    private static final String POINTCUT = "execution(* com.vbelova.aspect.ElectionService.vote(..))";
 
+    @Before(POINTCUT)
     public void before(JoinPoint joinPoint){
         String name = (String) joinPoint.getArgs()[0];
         logger.info("Before voting for {}", name);
     }
+
+    @After(POINTCUT)
     public void after(){
         logger.info("Voting completed");
     }
 
+    @AfterReturning(POINTCUT)
     public void afterReturning(){
         logger.info("Voting completed with result");
     }
 
+    @AfterThrowing(POINTCUT)
     public void afterThrowing(){
         logger.info("Exception during voting");
     }
 
+    @Around(POINTCUT)
     public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
@@ -34,4 +42,5 @@ public class Aspect {
         logger.info("method call {} took {} ms", joinPoint.getSignature().toShortString(), stopWatch.getTotalTimeMillis());
         return result;
     }
+
 }
--- src/main/java/com/vbelova/aspect/Main.java
+++ src/main/java/com/vbelova/aspect/Main.java
@@ -6,7 +6,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("spring.xml");
+        ApplicationContext appContext;
+        appContext = new ClassPathXmlApplicationContext("spring.xml");
+//        appContext = new AnnotationConfigApplicationContext(Spring.class);
+
         ElectionService someService = appContext.getBean(ElectionService.class);
         someService.vote("Marcon");
     }
